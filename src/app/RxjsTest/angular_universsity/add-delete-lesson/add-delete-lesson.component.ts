c98abc2736c28e81bbc0c12aafd37279ba42868f
morninng <mr_morninng_star_dash@yahoo.co.jp>
2016-08-03 22:53:01 +0900

    angular university format has been added

---
+++ src/app/RxjsTest/angular_universsity/add-delete-lesson/add-delete-lesson.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-add-delete-lesson',
+  templateUrl: 'add-delete-lesson.component.html',
+  styleUrls: ['add-delete-lesson.component.css']
+})
+export class AddDeleteLessonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
